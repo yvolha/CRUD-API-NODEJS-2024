d0dbad5b84a9cdd7b10de20aad69481b6a344b7e
Volha Yakauleva <yvvolha@gmail.com>
2024-10-16 23:57:20 +0300

    feat: add response codes

---
+++ src/components/request-handler.type.ts
@@ -0,0 +1,5 @@
+export enum IRequestRequiredProperties {
+    ID = 'id',
+    USERNAME = 'username',
+    HOBBIES = 'hobbies',
+}
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,3 +1,7 @@
 import { IDatabase } from "./database.type";
 
 export const DATABASE: IDatabase = {};
+
+export const DATABASE_REQUIRED_PROPERTIES: string[] = [
+    'id', 'username', 'age', 'hobbies',
+]
