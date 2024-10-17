2235d60859eb726ef94709c02103279e5ada4be7
Volha Yakauleva <yvvolha@gmail.com>
2024-10-17 19:12:20 +0300

    feat: update database and post request typing

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,7 +1,3 @@
 import { IDatabase } from "./database.type";
 
 export const DATABASE: IDatabase = {};
-
-export const DATABASE_REQUIRED_PROPERTIES: string[] = [
-    'id', 'username', 'age', 'hobbies',
-]
