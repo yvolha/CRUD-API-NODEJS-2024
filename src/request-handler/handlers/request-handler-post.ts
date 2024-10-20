5cc995b684c5609eac143ea6b13012abe9042cc0
Volha Yakauleva <yvvolha@gmail.com>
2024-10-20 22:53:34 +0300

    feat: add data reception for post request

--- src/request-handler/handlers/request-handler-post.ts
+++ src/request-handler/handlers/request-handler-post.ts
@@ -1,9 +1,25 @@
 import { IncomingMessage, ServerResponse } from "node:http";
+import checkRequiredFields from "../../utils/check-required-fields";
+import { IPostRequestRequiredProps } from "../request-handler.type";
 
 export default function handleRequestPost (
     path: string,
     req: IncomingMessage,
     res: ServerResponse,
 ) {
+    let data = '';
 
+    req.on('data', (chunk) => {
+        if (chunk) {
+            data = data += chunk;
+        }
+    });
+
+    req.on('end', () => {
+        console.log(data);
+
+        if (checkRequiredFields(data as unknown as IPostRequestRequiredProps, res)) {
+
+        }
+    })
 }
