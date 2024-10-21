43be3076381ba5d1a31b8a9aaa875c819424ca7c
Volha Yakauleva <yvvolha@gmail.com>
2024-10-21 22:01:36 +0300

    feat: add some put request handling

---
+++ src/request-handler/handlers/request-handler-put.ts
@@ -0,0 +1,72 @@
+import { IncomingMessage, ServerResponse } from "node:http";
+import { v6 as uuidv6 } from 'uuid';
+
+
+import checkRequiredFields from "../../utils/check-required-fields";
+import { ENDPOINTS, IPostRequestRequiredProps } from "../request-handler.type";
+import { getParsedJson } from "../../utils/get-parsed-json";
+import { sendError } from "../../utils/send-error";
+import { RESPONSE_CODES } from "../../constants/response-codes.constant";
+import { getBadRequestInvalidMessage, getNotFoundMessage, getUnsupportedEndpointMessage, INCORRECT_JSON } from "../../utils/get-message";
+import { DATABASE } from "../../database/database";
+import { sendResponse } from "../../utils/send-response";
+import getStandardizedPath from "../../utils/get-standardized-path";
+import getLastUrlPart from "../../utils/get-last-url-part";
+import getIsUuidValid from "../../utils/get-is-uuid";
+
+export default function handleRequestPut (
+    path: string,
+    req: IncomingMessage,
+    res: ServerResponse,
+) {
+    if (getStandardizedPath(path) === ENDPOINTS.API_USERS_X) {
+        const userId = getLastUrlPart(path);
+
+        if (!getIsUuidValid(userId)) {
+            sendError(res, RESPONSE_CODES.BAD_REQUEST, getBadRequestInvalidMessage(userId));
+        } else {
+            const user = DATABASE[userId];
+
+            if (!user) {
+                sendError(res, RESPONSE_CODES.NOT_FOUND, getNotFoundMessage(userId));
+            } else {
+                let data = '';
+
+                req.on('data', (chunk) => {
+                    if (chunk) {
+                        data = data += chunk;
+                    }
+                });
+
+                req.on('end', () => {
+                    const parsedJson = getParsedJson(data);
+
+                    if (!parsedJson) {
+                        sendError(res, RESPONSE_CODES.BAD_REQUEST, INCORRECT_JSON);
+                        return;
+                    }
+
+                    if (checkRequiredFields(parsedJson as unknown as IPostRequestRequiredProps, res)) {
+                        const newUserUuid = uuidv6();
+                        const userData = parsedJson as unknown as IPostRequestRequiredProps;
+
+                        const newUser = {
+                            id: newUserUuid,
+                            ...userData,
+                        }
+
+                        DATABASE[newUserUuid] = newUser;
+
+                        sendResponse(res, newUser, RESPONSE_CODES.CREATED);
+                    }
+                })
+
+
+                sendResponse(res, user);
+            }
+        }
+
+    } else {
+        sendError(res, RESPONSE_CODES.NOT_FOUND, getUnsupportedEndpointMessage(path));
+    }
+}
--- src/utils/check-required-fields.ts
+++ src/utils/check-required-fields.ts
@@ -37,15 +37,15 @@ export default function checkRequiredFields(data: IPostRequestRequiredProps, res
     return true;
 }
 
-function getIsUsernameInCorrectFormat (data: IPostRequestRequiredProps) {
+export function getIsUsernameInCorrectFormat (data: IPostRequestRequiredProps) {
     return data[POST_REQ_REQUIRED_PROPS.USERNAME] && typeof data[POST_REQ_REQUIRED_PROPS.USERNAME] === 'string';
 }
 
-function getIsAgeInCorrectFormat (data: IPostRequestRequiredProps) {
+export function getIsAgeInCorrectFormat (data: IPostRequestRequiredProps) {
     return data[POST_REQ_REQUIRED_PROPS.AGE] && typeof data[POST_REQ_REQUIRED_PROPS.AGE] === 'number';
 }
 
-function getIsHobbiesInCorrectFormat (data: IPostRequestRequiredProps) {
+export function getIsHobbiesInCorrectFormat (data: IPostRequestRequiredProps) {
     return data[POST_REQ_REQUIRED_PROPS.HOBBIES]
     && Array.isArray(data[POST_REQ_REQUIRED_PROPS.HOBBIES])
     && data[POST_REQ_REQUIRED_PROPS.HOBBIES].every(item => typeof item === 'string');
